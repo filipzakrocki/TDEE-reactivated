1d75f41f8b5b7f4b1eedcf6e565b4f8280acecf0
Filip <42967255+filipzakrocki@users.noreply.github.com>
2020-01-08 17:28:18 +0000

    Update WeekRow.js

--- src/containers/Result/WeekRow/WeekRow.js
+++ src/containers/Result/WeekRow/WeekRow.js
@@ -6,11 +6,26 @@ import WeekRowWrapper from "../../../components/ResultRows/WeekRowWrapper/WeekRo
 const WeekRow = props => {
   const { weekNo, startDate } = props;
 
+  const generateDate = (date, weeksToAdd) => {
+    let startDate, days, months, years, daysToAdd, outputDate, formattedDate;
+
+    startDate = new Date(date);
+    days = startDate.getDate();
+    months = startDate.getMonth();
+    years = startDate.getYear() + 1900;
+    daysToAdd = weeksToAdd * 7 - 7;
+
+    outputDate = new Date(years, months, days + daysToAdd);
+    formattedDate = outputDate.toDateString().slice(3);
+
+    return formattedDate;
+  };
+
   return (
     <WeekRowWrapper>
       <div className="weekRow-entry">
         <p>Week {weekNo}</p>
-        <p>{startDate}</p>
+        <p>{generateDate(startDate, weekNo)}</p>
       </div>
       <div className="weekRow-entry noMobile">
         <p>kg</p>
