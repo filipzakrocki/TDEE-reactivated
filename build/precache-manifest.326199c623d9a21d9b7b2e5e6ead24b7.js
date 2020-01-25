4b378edf45e364242908c4ab8f2457bc5e7c5e50
Filip <42967255+filipzakrocki@users.noreply.github.com>
2020-01-25 17:49:08 +0000

    added /build to commitable items

---
+++ build/precache-manifest.326199c623d9a21d9b7b2e5e6ead24b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5403eb4061c12bb8323696dc5fb98a4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e518ae9643661bfe160",
+    "url": "/static/css/main.4cd52a1f.chunk.css"
+  },
+  {
+    "revision": "83ad056435e9aa73ba45",
+    "url": "/static/js/2.e8394d69.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/static/js/2.e8394d69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e518ae9643661bfe160",
+    "url": "/static/js/main.749b9453.chunk.js"
+  },
+  {
+    "revision": "5d4f52a9819558f2eea5",
+    "url": "/static/js/runtime-main.292bccbb.js"
+  }
+]);
