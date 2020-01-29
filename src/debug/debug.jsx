bad6a188c47fef25ed48d71e42a765f4da4467f1
Filip <42967255+filipzakrocki@users.noreply.github.com>
2020-01-29 12:00:18 +0000

    Test build to check netlify deployment process

--- src/debug/debug.jsx
+++ src/debug/debug.jsx
@@ -1,46 +1,45 @@
-import React, {useState} from 'react';
-import {connect} from 'react-redux';
-import axios from 'axios'
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import axios from "axios";
 
-const Debugg = (props) => {
+const Debugg = props => {
+  const { state } = props;
+  const [user, setUser] = useState("");
 
-    const {state} = props;
-    const [user, setUser] = useState('zakrofil');
+  const saveState = (userState, user) => {
+    let address = `https://tdee-fit.firebaseio.com/states/${user}.json`;
+    axios.put(address, userState).then(res => console.log("SUCCESS"));
+  };
 
-    const saveState = (userState, user) => {
-        let address = `https://tdee-fit.firebaseio.com/states/${user}.json`
-        axios.put(address, userState)
-        .then(res => console.log('SUCCESS'));
-    }
+  const loadState = user => {
+    let address = `https://tdee-fit.firebaseio.com/states/${user}.json`;
+    axios
+      .get(address)
+      .then(res => localStorage.setItem("state", JSON.stringify(res.data)))
+      .then(res => window.location.reload());
+  };
 
-    const loadState = (user) => {
-        let address = `https://tdee-fit.firebaseio.com/states/${user}.json`;
-        axios.get(address)
-        .then(res => localStorage.setItem('state', JSON.stringify(res.data)))
-        .then(res => window.location.reload())
-    }
-
-    return (
-        <div>
-            <input type='text' value={user} onChange={e => setUser(e.target.value)}/>
-            <button onClick={() => localStorage.clear()}>CLEAR THE STORAGE</button>
-            <button onClick={() => console.log(state)}>CONSOLE.LOG THE STATE</button>
-            <button onClick={() => saveState(state, user)}>SAVE STATE TO FIREBASE</button>
-            <button onClick={() => loadState(user)}>LOAD STATE FROM FIREBASE</button>
-        </div>
-    );
-}
+  return (
+    <div>
+      <input type="text" value={user} onChange={e => setUser(e.target.value)} />
+      <button onClick={() => localStorage.clear()}>CLEAR THE STORAGE</button>
+      <button onClick={() => console.log(state)}>CONSOLE.LOG THE STATE</button>
+      <button onClick={() => saveState(state, user)}>
+        SAVE STATE TO FIREBASE
+      </button>
+      <button onClick={() => loadState(user)}>LOAD STATE FROM FIREBASE</button>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
-    return {
-        state: state.calculator
-    }
-}
+  return {
+    state: state.calculator
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-
-    };
-}
+  return {};
+};
 
 export default connect(mapStateToProps, null)(Debugg);
